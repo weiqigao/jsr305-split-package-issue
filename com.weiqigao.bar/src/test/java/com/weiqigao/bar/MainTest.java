13ac23678192077dae8d2f19da310b638dadf36f
Weiqi Gao <weiqigao@gmail.com>
2018-12-28 21:20:52 -0600

    Upgraded moduleplugin to 1.4.0, which added patch module support and resolved all issues illustrated by this project.  Added JUnit 5 tests.

---
+++ com.weiqigao.bar/src/test/java/com/weiqigao/bar/MainTest.java
@@ -0,0 +1,12 @@
+package com.weiqigao.bar;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        Main main = new Main();
+        assertNotNull(main);
+    }
+}
